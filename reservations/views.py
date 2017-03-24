9ffc0fe743977aa6e8f843802ec78ee71efaf934
sam <shammihembrom7@gmail.com>
2017-03-24 06:52:19 +0530

    added filter reservations with customer id | added gitignore file

--- reservations/views.py
+++ reservations/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render_to_response
 from rest_framework import viewsets
+from rest_framework import filters
 
 from reservations.models import Reservation
 from reservations.serializers import ReservationSerializer
@@ -7,3 +8,4 @@ from reservations.serializers import ReservationSerializer
 class ReservationViewSet(viewsets.ModelViewSet):
     queryset = Reservation.objects.all()
     serializer_class = ReservationSerializer
+    filter_fields = ('customer')
